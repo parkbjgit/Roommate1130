02a2e6ffd36fb69334dbe2e7422f5a54c9d37478
asfff0321 <afff0321@naver.com>
2022-11-30 10:52:22 +0900

    combine login mainpage

---
+++ OpenSourceProject/app/src/main/java/com/example/roommate/message_page.java
@@ -0,0 +1,24 @@
+package com.example.roommate;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class message_page extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_message_page, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
